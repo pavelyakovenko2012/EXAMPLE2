b734d06a0933efc10895861b064edcb2d5296381
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-07-10 17:46:04 +0500

    Найти индекс первго нужного элемента

---
+++ Ex008/Ex009/Ex010/Program.cs
@@ -0,0 +1,18 @@
+﻿int [] array = {14,23,58,13,46,58,56,79,98};
+
+
+int n = array.Length;
+int find = 58;
+
+int index = 0;
+
+while (index < n)
+{
+    
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index = index +1;
+}
