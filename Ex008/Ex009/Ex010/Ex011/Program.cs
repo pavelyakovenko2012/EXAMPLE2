a5d688227faf2ea211f9a22e3038365ee2d25b43
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-07-10 18:31:52 +0500

    Найти индекс первой 4ки

---
+++ Ex008/Ex009/Ex010/Ex011/Program.cs
@@ -0,0 +1,49 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index = index + 1;
+    }
+}
+
+
+void PrintArray(int[] fol)
+{
+    int count = fol.Length;
+    int position = 0;
+    while (position < count)
+    {
+       Console.WriteLine(fol[position]);
+       position++; 
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+           position = index;
+           break;
+        }
+        index++;
+    }
+    return position;
+}
+
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
